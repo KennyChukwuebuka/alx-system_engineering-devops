b5335f61bd6e0b0c5afabf8012d41102501b0fd0
kennyChukwuebuka <kenkinado@gmail>
2023-12-29 19:38:22 +0100

    file config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# using puppet to make changes to config file
+
+file_line { 'Turn off passwd auth':
+ ensure =>  'present',
+ path   =>  '/etc/ssh/ssh_config',
+ line   =>  'PasswordAuthentication no',
+}
+file_line { 'Declare file':
+ ensure =>  'present',
+ path   =>  '/etc/ssh/ssh_config',
+ line   =>  'IdentityFile ~/.ssh/school',
+}
