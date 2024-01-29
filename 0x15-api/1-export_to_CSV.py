e95d6646ccd8abe6914e045eff704bc7a4d1678f
kennyChukwuebuka <kenkinado@gmail>
2024-01-29 22:45:27 +0100

    added csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,21 +7,22 @@ File name must be: USER_ID.csv
 """
 
 if __name__ == "__main__":
+
     import csv
     import requests
     import sys
 
-    id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
-    response = requests.get(url)
-    name = response.json().get('name')
+    userId = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
+    name = user.json().get('username')
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
 
-    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(id)
-    response = requests.get(url)
-    tasks = response.json()
-
-    with open("{}.csv".format(id), 'w') as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for task in tasks:
-            writer.writerow([id, name, task.get('completed'),
-                             task.get('title')])
+    filename = userId + '.csv'
+    with open(filename, mode='w') as f:
+        writer = csv.writer(f, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL, lineterminator='\n')
+        for task in todos.json():
+            if task.get('userId') == int(userId):
+                writer.writerow([userId, name, str(task.get('completed')),
+                                 task.get('title')])
