573b9898ca2357f9cdfb6d7a616377b608172391
kennyChukwuebuka <kenkinado@gmail>
2024-02-15 14:08:23 +0100

    adv task

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Write a recursive function that queries the Reddit API,
+parses the title of all hot articles, and prints a
+sorted count of given keywords (case-insensitive,
+delimited by spaces. Javascript should count as
+javascript, but java should not).
+"""
+
+def count_words(subreddit, word_list):
+    """function that queries the Reddit API
+    and returns a sorted count of given keywords
+    """
+    pass
