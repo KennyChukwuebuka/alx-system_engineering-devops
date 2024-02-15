51ff5918895322c789f85699367090019208c10c
kennyChukwuebuka <kenkinado@gmail>
2024-02-15 16:07:27 +0100

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,17 +3,12 @@
 and returns the number of subscribers
 """
 import requests
-import sys
-import json
 
 
 def number_of_subscribers(subreddit):
     """function that queries the Reddit API
     and returns the number of subscribers
     """
-    username = 'kenny123'
-    password = 'kenny72'
-    user_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
