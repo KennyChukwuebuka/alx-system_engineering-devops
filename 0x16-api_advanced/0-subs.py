85b2545b9b48bfded1ada6ec422d0a54fe2dc83c
kennyChukwuebuka <kenkinado@gmail>
2024-03-17 01:17:58 +0100

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API
+"""Write a function that queries the Reddit API 
 and returns the number of subscribers
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """function that queries the Reddit API
+    """ function that queries the Reddit API
     and returns the number of subscribers
     """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+
     headers = {'User-Agent': 'Mozilla/5.0'}
+
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
+        return response.json()['data']
     else:
         return 0
